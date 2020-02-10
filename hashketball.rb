ba0612425756c0fce9f46145f121c5432bbfc0ce
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-10 19:22:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,6 +17,50 @@ def game_hash
         :blocks => 0,
         :slam_dunks => 0
       },
+      { 
+        :player_name => '',
+        :number => 0,
+        :shoe => 0,
+        :points => 0,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0
+      },
+      { 
+        :player_name => '',
+        :number => 0,
+        :shoe => 0,
+        :points => 0,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0
+      },
+      { 
+        :player_name => '',
+        :number => 0,
+        :shoe => 0,
+        :points => 0,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0
+      },
+      { 
+        :player_name => '',
+        :number => 0,
+        :shoe => 0,
+        :points => 0,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0
+      },      
     ]
   },
   :away => {
