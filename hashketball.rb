3aeccbd69b59cc2e9a076eaf3b2699ef2b0e7508
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-10 19:23:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,6 +3,67 @@ data = {:meow => 'kitten'}
 def game_hash
 {
   :home => {
+    :team_name => 'Brooklyn Nets',
+    :colors => ['Black', 'White'],
+    :players => [
+      { 
+        :player_name => 'Alan Anderson',
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0
+      },
+      { 
+        :player_name => '',
+        :number => 0,
+        :shoe => 0,
+        :points => 0,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0
+      },
+      { 
+        :player_name => '',
+        :number => 0,
+        :shoe => 0,
+        :points => 0,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0
+      },
+      { 
+        :player_name => '',
+        :number => 0,
+        :shoe => 0,
+        :points => 0,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0
+      },
+      { 
+        :player_name => '',
+        :number => 0,
+        :shoe => 0,
+        :points => 0,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0
+      },      
+    ]
+  },
+  :away => {
     :team_name => '',
     :colors => [],
     :players => [
@@ -62,10 +123,5 @@ def game_hash
         :slam_dunks => 0
       },      
     ]
-  },
-  :away => {
-    :team_name => '',
-    :colors => [],
-    :players => []
   }
 } end
